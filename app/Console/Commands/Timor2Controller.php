b4676a845443dc7000de4efccf86d9cdb28d6d65
timor <773791379@qq.com>
2021-01-30 14:28:35 +0800

    配置目录级别的代码风格检查

---
+++ app/Console/Commands/Timor2Controller.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 小小徐
+ * Date: 2021/1/30
+ * Time: 13:32
+ */
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Timor2Controller extends Command
+{
+    public function timr()
+    {
+        print_r(['aaa','aaaaaaa']);
+        echo 'aaa';
+    }
+}
