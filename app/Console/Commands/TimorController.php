9c002c46b6edef9c85667afa5b5239d8a6a506ea
timor <773791379@qq.com>
2021-01-30 14:03:20 +0800

    init lumen5.5

---
+++ app/Console/Commands/TimorController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 小小徐
+ * Date: 2021/1/30
+ * Time: 13:32
+ */
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TimorController extends Command
+{
+    public function timr()
+    {
+        echo 'aaa';
+    }
+}
